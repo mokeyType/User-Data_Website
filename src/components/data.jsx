0019e730ee6363b0603c51e1f9f43ca714f660ee
mokeyType <asmehta667@gmail.com>
2025-10-06 15:57:25 +0530

    size fixed in search

--- src/components/data.jsx
+++ src/components/data.jsx
@@ -4,7 +4,7 @@ const data = ({user,index}) => {
  
  if(user.name!='' && user.age !='' && user.email!= ''){
    return (
-    <div className="flex h-[15%] w-[98%] md:text-[1.5rem] text-[1rem] rounded-[20px] inset-shadow-[2px_2px_5px_rgba(0,0,0,0.5),-2px_-2px_3px_rgb(216,215,215)] bg-yellow-100 self-center">
+    <div className="flex h-[15%] w-[98%] md:text-[1.5rem] text-[0.8rem] rounded-[20px] inset-shadow-[2px_2px_5px_rgba(0,0,0,0.5),-2px_-2px_3px_rgb(216,215,215)] bg-yellow-100 self-center">
           <p className="w-1/4 flex justify-center items-center">{index}</p>
           <p className="w-1/4 flex justify-center items-center primary_key">{user.name}</p>
           <p className="w-1/4 flex justify-center items-center">{user.email}</p>
